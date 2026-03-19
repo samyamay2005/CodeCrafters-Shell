7dcd332a47c142ac80433f885904b8fa958af6d7
Samyamay <samyamay2005@gmail.com>
2026-03-19 20:51:19 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <unistd.h>
-#include <cstdlib>
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<unistd.h>
+#include<cstdlib>
+#include<vector>
+#include <sys/wait.h>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -29,25 +31,53 @@ int main() {
     continue;
     }
 
-  char* pathEnv = getenv("PATH");
-  std::string pathStr = pathEnv;
-  std::stringstream ss(pathStr);
-  std::string dir;
+    char* pathEnv = getenv("PATH");
+    std::string pathStr = pathEnv;
+    std::stringstream ss(pathStr);
+    std::string dir;
 
-  while(std::getline(ss, dir, ':')){
-    std::string fullPath = dir + "/" + msg;
+    while(std::getline(ss, dir, ':')){
+      std::string fullPath = dir + "/" + msg;
 
-    if(access(fullPath.c_str(), X_OK) == 0){
-      std::cout<<msg<<" is "<<fullPath<<std::endl;
-      goto found;
+      if(access(fullPath.c_str(), X_OK) == 0){
+        std::cout<<msg<<" is "<<fullPath<<std::endl;
+        goto found;
+      }
     }
-  }
 
-  std::cout<<msg<<": not found"<<std::endl;
+    std::cout<<msg<<": not found"<<std::endl;
 
-  found:
-  continue;
-}
-    std::cout<<command<<": command not found"<<std::endl;
+    found:
+    continue;
+    }
+    std::vector<std::string> tokens;
+    std::stringstream ss(command);
+    std::string word;
+
+    while (ss >> word) {
+      tokens.push_back(word);
+    }
+
+    // Convert to char* array
+    std::vector<char*> args;
+    for (auto& token : tokens) {
+      args.push_back(&token[0]);
+    }
+    args.push_back(nullptr);
+
+    // Create child process
+    pid_t pid = fork();
+
+    if (pid == 0) {
+    // Child process executes command
+    execvp(args[0], args.data());
+
+    // If exec fails
+    std::cout << tokens[0] << ": command not found" << std::endl;
+    exit(1);
+    } else {
+      // Parent waits
+      waitpid(pid, NULL, 0);
+    }
   }
 }
